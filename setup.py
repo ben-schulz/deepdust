c9d2a29a9416ff5537ea92d37d9434a0f6a89eba
ben-schulz <benjamin.john.schulz@gmail.com>
2018-09-30 01:28:58 -0500

    feat(build): create spec command.
    Create custom command for setuptools
    that invokes scripts to fetch
    current JSON-LD test manifest
    and regenerates test source.

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 import setuptools
+import subprocess
+
+import distutils.cmd
+
+class BuildSpecTestCommand(distutils.cmd.Command):
+
+    description = 're-generate spec tests from remote manifest.'
+
+    user_options = [
+        #format: (longarg, shortarg, description)
+        ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        cmd = ['python3', 'deepdust/test/spec/get_latest.py']
+        subprocess.check_call(cmd)
+
 
 def readme():
     return ''
+
 
 setuptools.setup(
     name='deepdust',
@@ -28,6 +51,12 @@ setuptools.setup(
 
     test_suite='deepdust.test.test_all',
 
+    cmdclass={
+        'jsonld_spec': BuildSpecTestCommand
+        },
+    
     include_package_data=True,
     zip_safe=False
 )
+
+
